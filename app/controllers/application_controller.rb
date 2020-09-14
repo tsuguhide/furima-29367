5ca439a06818ca51f61c6a455e373e32c125e87d
tsuguhide <tsuguhide.n@gmail.com>
2020-09-14 21:40:53 +0900

    バリデーション・ルーティング等

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,36 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def index
+  end
+
+  def create
+    @user = User.new(configure_permitted_parameter)
+
+    if @user.valid?
+      @user.save  # バリデーションをクリアした時
+      return redirect_to root_path
+    else
+      render "new"  # バリデーションに弾かれた時
+    end
+  end
 
   private
+
+ # def user_params
+ #   params.require(:user).permit(:nick_name, :family_name, :first_name)
+ # end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_FURIMA_USER"] && password == ENV["BASIC_FURIMA_PASSWORD"]      
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: 
+      [:nick_name, :family_name, :first_name, :furigana_family_name, :furigana_first_name, :birthday])
+  end
+
 end
