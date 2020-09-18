c88c7c403423bbe26d3567690f5bed6d57879dad
tsuguhide <tsuguhide.n@gmail.com>
2020-09-18 20:27:39 +0900

    降順の設定、ダミー画像の削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
-    @item = Item.all
+    @item = Item.all.order("created_at DESC")
   end
 
   private
