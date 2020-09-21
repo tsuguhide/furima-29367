f2812a3238771e5e6cf2da4cd94b2fed073027b9
tsuguhide <tsuguhide.n@gmail.com>
2020-09-21 14:42:58 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,9 +17,20 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
 
